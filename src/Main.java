779c1f042eb5830cdf1abc92d45d342ed9b862dd
Amin Hosseini <amin.hs117@gmail.com>
2020-07-02 11:27:39 +0200

    second commit until A6.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.util.Scanner;
+
+import java.util.*;
 public class Main {
 
     // ---------------------------------------- A1 ------------------------------------------------
@@ -33,11 +34,11 @@ public class Main {
 
         if (startNum % 2 ==0){
             for(int i=startNum+1; i<endNum; i = i+2){
-                System.out.print(i + ", ");
+                System.out.print(i + " ");
             }
         } else{
             for(int i = startNum; i<endNum; i = i+2){
-                System.out.print(i + ", ");
+                System.out.print(i + " ");
             }
         }
     }
@@ -58,21 +59,89 @@ public class Main {
         System.out.printf("Sum is %d.", result);
     }
 
-
-
     // ---------------------------------------- A5 ------------------------------------------------
 
+    public static void findX(){
+        int[] myArray = {1,2,2,3,1,8,9,7,2,9};
+        int counterNum = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter your guess number between 0 and 10: ");
+        int x = in.nextInt();
 
-
-
+        for(int i : myArray){
+            if(x == i){
+                counterNum++;
+            }
+        }
+        if(counterNum == 0){
+            System.out.println("Nothing found!");
+        } else{
+            System.out.printf("The number '%d' occurs %d times in following array.\n", x, counterNum);
+            System.out.println(Arrays.toString(myArray));
+        }
+    }
 
 
 
     // ---------------------------------------- A6 ------------------------------------------------
 
+    public static void fillAndPrint(){
+        int[] myArray = {1,4,5,2,8,9,12,44,52,15};
+        for(int i: myArray){
+            System.out.print(i + " ");
+        }
+    }
+
+
+    // ---------------------------------------- A7 ------------------------------------------------
+
+    public static void printArrayInReverse(){
+        int[] myArray;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter 10 numbers: ");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+
+
+        System.out.println(num1 + " " + num2);
+
+    }
+
+
+
+
+    // ---------------------------------------- A8 ------------------------------------------------
+
+
+
+
+
+    // ---------------------------------------- A9 ------------------------------------------------
+
+
+
+
+
+    // ---------------------------------------- A10 ------------------------------------------------
+
+
+
+
+
+    // ---------------------------------------- A11 ------------------------------------------------
+
+
+
+
+
+    // ---------------------------------------- A12 ------------------------------------------------
+
+
+
+
 
 
     public static void main(String[] args) {
-        sumCal();
+        oddNumInYourRange();
     }
 }
