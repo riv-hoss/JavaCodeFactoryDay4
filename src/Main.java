c11896ea0d3c0719d9493d52c66ad6ecadae3336
Amin Hosseini <amin.hs117@gmail.com>
2020-07-02 10:01:14 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class Main {
+
+    // ---------------------------------------- A1 ------------------------------------------------
+
+    public static void loopCounter(){
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i);
+
+        }
+    }
+
+
+    // ---------------------------------------- A2 ------------------------------------------------
+
+    public static void evenNumCounter(){
+        for(int i = 2; i <= 100; i = i+2){
+            System.out.println(i);
+
+        }
+    }
+
+
+
+    // ---------------------------------------- A3 ------------------------------------------------
+
+    public static void oddNumInYourRange(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a starting number: ");
+        int startNum = in.nextInt();
+        System.out.print("Please enter an ending number: ");
+        int endNum = in.nextInt();
+
+        if (startNum % 2 ==0){
+            for(int i=startNum+1; i<endNum; i = i+2){
+                System.out.print(i + ", ");
+            }
+        } else{
+            for(int i = startNum; i<endNum; i = i+2){
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+
+
+
+    // ---------------------------------------- A4 ------------------------------------------------
+
+    public static void sumCal(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Until what? ");
+        int inp = in.nextInt();
+        for(int i = 1; i <= inp; i++){
+            int result = 0;
+            result = result +i;
+            System.out.printf("Sum is %d", result);
+        }
+    }
+
+
+
+    // ---------------------------------------- A5 ------------------------------------------------
+
+
+
+
+
+
+
+    // ---------------------------------------- A6 ------------------------------------------------
+
+
+
+    public static void main(String[] args) {
+        sumCal();
+    }
+}
